8e4e5ac5c104700f87901d8a5c87ac67f753fe6d
jermzblake <jermzblake@gmail.com>
2025-11-02 09:59:37 -0700

    add routes and return RouterProvider in App

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,3 +1,5 @@
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './routes'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/client/components/ui/card'
 import { APITester } from './APITester'
 import '.././index.css'
@@ -6,36 +8,9 @@ import logo from '.././logo.svg'
 import reactLogo from '.././react.svg'
 import { SignInButton } from '@/client/components/ui/button'
 
+// Register Providers here if needed
 export function App() {
-  return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
-        />
-      </div>
-      <SignInButton />
-      <Card>
-        <CardHeader className="gap-4">
-          <CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
-          <CardDescription>
-            Edit <code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">src/App.tsx</code> and save to
-            test HMR
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <APITester />
-        </CardContent>
-      </Card>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
