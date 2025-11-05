efc0a927b82a44eb4f08e7c59c9631c3f3889f30
jermzblake <jermzblake@gmail.com>
2025-11-05 15:12:08 -0700

    update dashboard to take in updated TaskForm

--- src/client/pages/dashboard.tsx
+++ src/client/pages/dashboard.tsx
@@ -1,11 +1,18 @@
 import { useAuth } from '../auth/AuthContext'
+import { TaskForm } from '@/client/components/task-form'
 
 export const DashboardPage = () => {
   const { logout } = useAuth()
   return (
     <div>
-      <h1>Welcome to the Dashboard</h1>
-      <button onClick={logout}>Logout</button>
+      <div className="flex justify-between items-center mb-6">
+        <h1>Welcome to the Dashboard</h1>
+        <p>|</p>
+        <button onClick={logout}>Logout</button>
+      </div>
+      <div className="flex flex-col gap-6 bg-white p-6 rounded-lg shadow-md">
+        <TaskForm />
+      </div>
     </div>
   )
 }
