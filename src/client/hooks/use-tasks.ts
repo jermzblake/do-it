bf034c6ee3616d00e08f458818b99942845a5108
jermzblake <jermzblake@gmail.com>
2025-11-07 10:17:33 -0700

    create initial tasks component

--- src/client/hooks/use-tasks.ts
+++ src/client/hooks/use-tasks.ts
@@ -2,12 +2,10 @@ import { fetchTasksByStatus } from '@/client/services/tasks.service'
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import type { TasksByStatusProps } from '@/types/tasks.types'
 
-const useTasksByStatus = ({ status, userId, page, pageSize = 5 }: TasksByStatusProps) => {
+export const useTasksByStatus = ({ status, userId, page, pageSize = 5 }: TasksByStatusProps) => {
   return useQuery({
     queryKey: ['tasks', status, userId, page, pageSize],
     queryFn: () => fetchTasksByStatus({ status, userId, page, pageSize }),
     placeholderData: keepPreviousData,
   })
 }
-
-export default useTasksByStatus
