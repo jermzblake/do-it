1aeff4eaad4d86b85b98a2d8c4937bffb1bdd2a9
jermzblake <jermzblake@gmail.com>
2025-11-07 18:01:03 -0700

    add tasks keys

--- src/client/hooks/use-tasks.ts
+++ src/client/hooks/use-tasks.ts
@@ -2,9 +2,17 @@ import { fetchTasksByStatus } from '@/client/services/tasks.service'
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import type { TasksByStatusProps } from '@/types/tasks.types'
 
+export const tasksKeys = {
+  all: ['tasks'],
+  lists: () => [...tasksKeys.all, 'list'],
+  statusList: (params: TasksByStatusProps) => [...tasksKeys.lists(), params],
+  byId: (id: string) => [...tasksKeys.all, id],
+  byUserId: (userId: string) => [...tasksKeys.all, userId],
+}
+
 export const useTasksByStatus = ({ status, userId, page, pageSize = 5 }: TasksByStatusProps) => {
   return useQuery({
-    queryKey: ['tasks', status, userId, page, pageSize],
+    queryKey: tasksKeys.statusList({ status, userId, page, pageSize }),
     queryFn: () => fetchTasksByStatus({ status, userId, page, pageSize }),
     placeholderData: keepPreviousData,
   })
