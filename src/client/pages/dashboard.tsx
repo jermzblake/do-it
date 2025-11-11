03093940e37c8e92b4281ac1b4d75fb825438d0a
jermzblake <jermzblake@gmail.com>
2025-11-11 14:08:24 -0700

    add temporary UI for Tasks and task creation button/form

--- src/client/pages/dashboard.tsx
+++ src/client/pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from '../auth/AuthContext'
+import { CreateTaskContainer } from '@/client/components/create-task-container'
 import { TaskForm } from '@/client/components/task-form'
+import { Tasks } from '@/client/components/tasks'
 
 export const DashboardPage = () => {
   const { logout } = useAuth()
@@ -11,7 +13,11 @@ export const DashboardPage = () => {
         <button onClick={logout}>Logout</button>
       </div>
       <div className="flex flex-col gap-6 bg-white p-6 rounded-lg shadow-md">
-        <TaskForm />
+        {/* <TaskForm /> */}
+        <CreateTaskContainer />
+      </div>
+      <div>
+        <Tasks />
       </div>
     </div>
   )
