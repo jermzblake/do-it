9347da92a45ca528d4a0ed3c4e8ef83ee9f3a1e7
jermzblake <jermzblake@gmail.com>
2025-11-12 16:46:29 -0700

    refactor dashboard page to have just SiteHeader and TaskDashboard

--- src/client/pages/dashboard.tsx
+++ src/client/pages/dashboard.tsx
@@ -1,23 +1,12 @@
-import { useAuth } from '../auth/AuthContext'
-import { CreateTaskContainer } from '@/client/components/create-task-container'
-import { TaskForm } from '@/client/components/task-form'
-import { Tasks } from '@/client/components/tasks'
+import { SiteHeader } from '../components/site-header'
+import TaskDashboard from '../components/task-dashboard'
 
 export const DashboardPage = () => {
-  const { logout } = useAuth()
   return (
-    <div>
-      <div className="flex justify-between items-center mb-6">
-        <h1>Welcome to the Dashboard</h1>
-        <p>|</p>
-        <button onClick={logout}>Logout</button>
-      </div>
-      <div className="flex flex-col gap-6 bg-white p-6 rounded-lg shadow-md">
-        {/* <TaskForm /> */}
-        <CreateTaskContainer />
-      </div>
+    <div className="[--header-height:calc(--spacing(14))]">
+      <SiteHeader pageTitle="Dashboard" />
       <div>
-        <Tasks />
+        <TaskDashboard />
       </div>
     </div>
   )
