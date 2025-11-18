4595f98f350e85c2cf6fd62edcc3fbe260b1fad8
jermzblake <jermzblake@gmail.com>
2025-11-18 14:40:26 -0700

    update landing page text

--- src/client/pages/landing.tsx
+++ src/client/pages/landing.tsx
@@ -6,7 +6,9 @@ export const LandingPage = () => {
 
   return (
     <div>
-      <h1>Welcome to the Landing Page</h1>
+      <h1 className="text-4xl font-bold mb-8 text-center">
+        Do It <span className="text-blue-900">NOW</span>
+      </h1>
       <SignInButton />
     </div>
   )
