49a265264d49ae963debe4f6eaad5b0be7a8d51c
jermzblake <jermzblake@gmail.com>
2025-11-17 22:06:21 -0700

    remove unused imports from App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,7 @@
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './routes'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/client/components/ui/card'
-import { APITester } from './APITester'
 import '.././index.css'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-
-import logo from '.././logo.svg'
-import reactLogo from '.././react.svg'
-import { SignInButton } from '@/client/components/ui/button'
 
 const queryClient = new QueryClient()
 
