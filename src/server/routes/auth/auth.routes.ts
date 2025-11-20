ae3c7c11b95bec7f7172e34858d84320a1b9bde8
jermzblake <jermzblake@gmail.com>
2025-11-19 20:48:04 -0700

    remove unused code

--- src/server/routes/auth/auth.routes.ts
+++ src/server/routes/auth/auth.routes.ts
@@ -1,8 +1,4 @@
-import { randomUUID } from 'crypto'
 import * as AuthController from '../../controllers/auth/auth.controller.ts'
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
-const REDIRECT_URI = 'http://localhost:3000/api/auth/callback'
 
 export const authRoutes = {
   '/api/auth/google': {
