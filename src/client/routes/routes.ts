fbfb614354d201ee589172a608d10d8fdb9b1070
jermzblake <jermzblake@gmail.com>
2025-11-21 12:07:24 -0700

    add new create task page and route

--- src/client/routes/routes.ts
+++ src/client/routes/routes.ts
@@ -3,4 +3,5 @@ export const routes = {
   dashboard: '/dashboard',
   taskDetails: (taskId: string) => `/tasks/${taskId}`,
   taskDetailsPattern: '/tasks/$taskId',
+  createTask: '/tasks/create',
 } as const
