95e2c822d842f11308f4f2903032030556ebf820
jermzblake <jermzblake@gmail.com>
2026-04-05 18:00:49 -0600

    feat: import React in button-group component for compatibility

--- src/client/components/ui/button-group.tsx
+++ src/client/components/ui/button-group.tsx
@@ -1,5 +1,6 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 import { Slot } from '@radix-ui/react-slot'
+import * as React from 'react'
 
 import { cn } from '@/client/lib/utils'
 import { Separator } from '@/client/components/ui/separator'
